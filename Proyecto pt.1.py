232101d00f04ffee08183375edb687be1ae73b3c
Paola Ramos <82003617+Paolinchi03@users.noreply.github.com>
2023-03-10 07:47:28 -0600

    Add files via upload

---
+++ Proyecto pt.1.py
@@ -0,0 +1,16 @@
+#Proyecto Programación
+
+
+
+#Ingreso de datos para registrar al usuario
+print ("Bienvenidx a ENTREGAS MEX, para registrarse como usuario de nuestro servicio, por favor ingrese los siguientes datos:")
+correocomercio= input("Ingrese el correo electrónico del comercio: ") #Almacena el correo del comercio
+telefonocomercio= int(input("Ingrese el número de teléfono del comercio: ")) #Almacena el número de teléfono del comercio
+dueñocomercio= input("Ingrese el nombre de la persona dueña del comercio: ") #Almacena el nombre de la persona dueña del comercio
+ubicacioncomercio= input("Ingrese la ubicación del comercio: ") #Almacena la ubicación del comercio
+
+#Ingreso de datos para registrar una factura electrónica
+
+
+
+#Ingreso de datos para creación de paquetes
