061f15c0664bad3657dba137e9cf9647c99786f0
AndresFonseca <123909684+fonseax@users.noreply.github.com>
2023-03-26 13:47:28 -0600

    Creación de datos para la factura electrónica

--- Proyecto pt.1.py
+++ Proyecto pt.1.py
@@ -10,6 +10,12 @@ dueñocomercio= input("Ingrese el nombre de la persona dueña del comercio: ") #
 ubicacioncomercio= input("Ingrese la ubicación del comercio: ") #Almacena la ubicación del comercio
 
 #Ingreso de datos para registrar una factura electrónica
+print ("Hola de nuevo, por favor ingrese los datos para su factura electronica")
+tipoDeCedula= input ("Ingrese el tipo de cedula:")
+numeroDeCedula= input ("Ingrese su numero de cedula:") #almacena el numero de cedula para la facturacion nombreRegistrado= input ("Ingrese el nombre con el que se registro:") #almacena el nombre con el que fue registrado telefono= input ('Ingrese el numero de telefono para su facturacion:") #almacena el telefono para la facturacion correo= input ("Ingrese el correo para su facturacion:") #almacena el correo respectivo para la facturacion
+provincia= input ("Provincia:") #almacena la provincia para la facturacion 
+canton= input ("Canton: ") #almacena el canton para la facturacion
+distrito= input ("Distrito:") #almacena el distrito para la facturacion 
 
 
 
